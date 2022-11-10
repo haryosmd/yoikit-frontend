be6715448afe8aac4930a2f94d0ef33d2a12baa9
haryosmd <haryosmd@gmail.com>
2022-11-10 17:59:23 +0700

    success view done

--- src/components/layouts/NavigationLinks.vue
+++ src/components/layouts/NavigationLinks.vue
@@ -16,7 +16,7 @@ import { RouterLink } from 'vue-router';
           Categories</a>
       </li>
       <li>
-        <RouterLink to="/"
+        <RouterLink to="/pricing"
           class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-yellow-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
           Pricing</RouterLink>
       </li>
